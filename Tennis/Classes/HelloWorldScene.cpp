899eadbcfb6ddf2887263f6d7a4eca6cb7a8d892
Yury Akatov <yuraaka@gmail.com>
2012-12-15 22:50:30 +0400

    tennis ball improved with a physics, left player is controlled by touch

--- Tennis/Classes/HelloWorldScene.cpp
+++ Tennis/Classes/HelloWorldScene.cpp
@@ -1,7 +1,9 @@
 #include "HelloWorldScene.h"
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
 
+#pragma warning (disable : 4355) // this in ctor
 #define THROW_UNLESS(expr) ThrowUnless(!!expr, #expr)
 
 namespace
@@ -15,11 +17,143 @@ namespace
       throw std::logic_error(msg);
     }
   }
+
+  bool IsContain(const CCSize& srcRect, const CCPoint& srcCenter, const CCPoint& targetPoint)
+  {
+    return 
+      (targetPoint.x > srcCenter.x - srcRect.width / 2)
+      && (targetPoint.x < srcCenter.x + srcRect.width / 2)
+      && (targetPoint.y > srcCenter.y - srcRect.height / 2)
+      && (targetPoint.y < srcCenter.y + srcRect.height / 2);
+  }
+  
+  class GLDrawFlagsGuard
+  {
+  public:
+    GLDrawFlagsGuard()
+    {
+      glDisable(GL_TEXTURE_2D);
+      glDisableClientState(GL_COLOR_ARRAY);
+      glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+//      glEnableClientState(GL_VERTEX_ARRAY);
+    }
+
+    ~GLDrawFlagsGuard()
+    {
+//       glDisableClientState(GL_VERTEX_ARRAY);
+      glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+      glEnableClientState(GL_COLOR_ARRAY);
+      glEnable(GL_TEXTURE_2D);
+    }
+  };
+
+  CCSprite& CreateSprite(const std::string& picturePath)
+  {
+    CCSprite* picture = CCSprite::create("Ball.png");
+    THROW_UNLESS(picture);
+    return *picture;
+  }
+
+  void AddEarth(CCPoint pos, const GameContext& context)
+  {
+    const Coordinator& coords = *context.Coords;
+    b2BodyDef def;
+    def.type = b2_staticBody;
+    def.allowSleep = true;
+    def.position = coords.ToBox2d(pos);
+    b2Body* earth = context.World->CreateBody(&def);
+    b2PolygonShape bottomEarth;
+    bottomEarth.SetAsBox(coords.ToBox2d(pos.x), coords.ToBox2d(5));
+    b2FixtureDef fixDef;
+    fixDef.density = 0;
+    fixDef.restitution = 1;
+    fixDef.shape = &bottomEarth;
+    fixDef.friction = 0;
+    earth->CreateFixture(&fixDef);
+  }
+  
+  void CreateEarth(const GameContext& context)
+  {
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    const Coordinator& coords = *context.Coords;
+    AddEarth(ccp(winSize.width / 2, -5), context);
+    AddEarth(ccp(winSize.width / 2, winSize.height + 5), context);
+  }
+}
+
+Coordinator::Coordinator()
+  : Koeff(CCDirector::sharedDirector()->getWinSize().width / 10)
+{
+  THROW_UNLESS(int(Koeff));
+}
+
+float Coordinator::ToBox2d(float cocos2dPos) const
+{
+  return cocos2dPos / Koeff;
+}
+
+float Coordinator::ToCocos2d(float box2dPos) const
+{
+  return box2dPos * Koeff;
+}
+
+b2Vec2 Coordinator::ToBox2d(const CCPoint& cocos2dPoint) const
+{
+  return b2Vec2(ToBox2d(cocos2dPoint.x), ToBox2d(cocos2dPoint.y));
+}
+
+b2Vec2 Coordinator::ToBox2d(const CCSize& size) const
+{
+  return b2Vec2(ToBox2d(size.width), ToBox2d(size.height));
+}
+
+float Coordinator::GetTransformKoeff() const
+{
+  return Koeff;
+}
+
+CCPoint Coordinator::ToCocos2d(const b2Vec2& pos) const
+{
+  return CCPoint(pos.x * Koeff, pos.y * Koeff);
+}
+
+TennisBall::TennisBall(CCPoint pos, const GameContext& context)
+  : Sprite(CreateSprite("Ball.png"))
+  , Body(NULL)
+  , Context(context)
+{
+  Sprite.setPosition(pos);
+  b2BodyDef bodyDef;
+  bodyDef.position = Context.Coords->ToBox2d(pos);
+  bodyDef.type = b2_dynamicBody;
+  bodyDef.fixedRotation = true;
+  Body = Context.World->CreateBody(&bodyDef);
+  b2PolygonShape shape;
+  b2Vec2 shapeSize = Context.Coords->ToBox2d(Sprite.getContentSize());
+  shape.SetAsBox(shapeSize.x / 2, shapeSize.y / 2);
+  b2FixtureDef fixDef;
+  fixDef.shape = &shape;
+  fixDef.restitution = 1;
+  fixDef.density = 10;
+  fixDef.friction = 0;
+  Body->CreateFixture(&fixDef);
+  Context.Layer->addChild(&Sprite, 1);
+  Body->ApplyLinearImpulse(Context.Coords->ToBox2d(ccp(0, -100)), Context.Coords->ToBox2d(pos));
+}
+
+void TennisBall::Update()
+{
+  Sprite.setPosition(Context.Coords->ToCocos2d(Body->GetPosition()));
 }
 
 HelloWorld::HelloWorld()
   : WindowSize(CCDirector::sharedDirector()->getWinSize())
+  , DbgDraw(Coords.GetTransformKoeff())
+  , World(b2Vec2(0, 0))
+  , Context(this, &Coords, &World)
 {
+  DbgDraw.SetFlags(b2Draw::e_shapeBit | b2Draw::e_jointBit | b2Draw::e_aabbBit | b2Draw::e_centerOfMassBit | b2Draw::e_pairBit);
+  World.SetDebugDraw(&DbgDraw);
 }
 
 CCScene* HelloWorld::scene()
@@ -49,12 +183,17 @@ bool HelloWorld::init()
   try
   {
     THROW_UNLESS(CCLayer::init());
+
+    setTouchEnabled(true);
+
     AddExitButton();
-    AddCaption();
+    //AddCaption();
     AddPlayer(true);
-    AddPlayer(false);
+    //AddPlayer(false);
     AddBall();
+    CreateEarth(Context);
     //AddBackground();
+    schedule(schedule_selector(HelloWorld::Tick));
     return true;
   }
   catch (const std::exception& err)
@@ -62,6 +201,48 @@ bool HelloWorld::init()
     CCLOGERROR("Init error: %s", err.what());
     return false;
   }
+}
+
+void HelloWorld::draw()
+{
+  CCLayer::draw();
+  //GLDrawFlagsGuard flags;
+  World.DrawDebugData();
+}
+
+void HelloWorld::ccTouchesEnded(CCSet* touches, CCEvent* event)
+{
+  int i = 0;
+}
+
+void HelloWorld::ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent)
+{
+  for (CCSetIterator it = pTouches->begin(); it != pTouches->end(); ++it)
+  {
+    CCTouch* touch = static_cast<CCTouch*>(*it);
+    if (!touch)
+    {
+      continue;
+    }
+
+    if (!IsContain(PlayerLeft->getContentSize(), PlayerLeft->getPosition(), touch->getPreviousLocation()))
+    {
+      continue;
+    }
+
+    float deltaY = touch->getDelta().y;
+    float playerNewY = PlayerLeft->getPositionY() + deltaY;
+    playerNewY = std::max(playerNewY, PlayerLeft->getContentSize().height / 2);
+    playerNewY = std::min(playerNewY, WindowSize.height - PlayerLeft->getContentSize().height / 2);
+    PlayerLeft->setPositionY(playerNewY);
+  }
+}
+
+void HelloWorld::Tick(float delta)
+{
+  World.Step(delta, 10, 10);
+  World.ClearForces();
+  Ball->Update();
 }
 
 void HelloWorld::AddExitButton()
@@ -95,27 +276,21 @@ void HelloWorld::AddBackground()
 
 void HelloWorld::AddPlayer(bool left)
 {
-  CCSprite* player = CCSprite::create("Player.png");
-  THROW_UNLESS(player);
-  const int playerX = left ? player->getContentSize().width / 2 : WindowSize.width - player->getContentSize().width / 2;
+  PlayerLeft = CCSprite::create("Player.png");
+  THROW_UNLESS(PlayerLeft);
+  const int playerX = left ? PlayerLeft->getContentSize().width / 2 : WindowSize.width - PlayerLeft->getContentSize().width / 2;
   const int playerY = WindowSize.height / 2;
-  player->setPosition(ccp(playerX, playerY));
-  addChild(player, 1);
+  PlayerLeft->setPosition(ccp(playerX, playerY));
+  addChild(PlayerLeft, 1);
 }
 
 void HelloWorld::AddBall()
 {
-  CCSprite* ball = CCSprite::create("Ball.png");
-  THROW_UNLESS(ball);
-  ball->setPosition(ccp(WindowSize.width / 2, WindowSize.height / 2));
-  addChild(ball, 1);
-  CCAction* toLeft = CCMoveTo::create(10, ccp(ball->getContentSize().width / 2, WindowSize.height / 2));
-  ball->runAction(toLeft);
+  Ball.reset(new TennisBall(ccp(WindowSize.width / 2, WindowSize.height / 2), Context));
 }
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
-    // "close" menu item clicked
-    CCDirector::sharedDirector()->end();
+  // "close" menu item clicked
+  CCDirector::sharedDirector()->end();
 }
-
--- Tennis/Classes/HelloWorldScene.h
+++ Tennis/Classes/HelloWorldScene.h
@@ -1,9 +1,55 @@
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
+#define NOMINMAX
 #include <cocos2d.h>
-#include <Box2D/Box2D.h>
+#include <Box2D\Box2D.h>
+#include <Box2D\Common\b2Math.h>
 #include <SimpleAudioEngine.h>
+#include <GLES-Render.h>
+
+class Coordinator
+{
+public:
+  Coordinator();
+
+  float ToBox2d(float cocos2dPos) const;
+  b2Vec2 ToBox2d(const cocos2d::CCPoint& cocos2dPoint) const;
+  b2Vec2 ToBox2d(const cocos2d::CCSize& size) const;
+  float ToCocos2d(float box2dPos) const;
+  cocos2d::CCPoint ToCocos2d(const b2Vec2& pos) const;
+  float GetTransformKoeff() const;
+
+private:
+  const float Koeff;
+};
+
+struct GameContext
+{
+  cocos2d::CCLayer* Layer;
+  const Coordinator* Coords;
+  b2World* World;
+
+  GameContext(cocos2d::CCLayer* myLayer, const Coordinator* myCoords, b2World* myWorld)
+    : Layer(myLayer)
+    , Coords(myCoords)
+    , World(myWorld)
+  {
+  }
+};
+
+class TennisBall
+{
+public:
+  TennisBall(cocos2d::CCPoint pos, const GameContext& svc);
+  //void EnableSyncWithBox2d(bool enable);
+  void Update();
+
+private:
+  cocos2d::CCSprite& Sprite;
+  b2Body* Body;
+  const GameContext Context;
+};
 
 class HelloWorld : public cocos2d::CCLayer
 {
@@ -13,6 +59,10 @@ public:
 //-- cocos2d::CCLayer
 public:
   virtual bool init();  
+  virtual void draw();
+
+  virtual void ccTouchesMoved(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+  virtual void ccTouchesEnded(cocos2d::CCSet* touches, cocos2d::CCEvent* event);
 
 public:
   static cocos2d::CCScene* scene();
@@ -26,8 +76,17 @@ private:
   void AddPlayer(bool left);
   void AddBall();
 
+  void Tick(float delta);
+
 private:
+  const Coordinator Coords;
   const cocos2d::CCSize WindowSize;
+  GLESDebugDraw DbgDraw;
+  b2World World;
+  const GameContext Context;
+
+  std::auto_ptr<TennisBall> Ball;
+  cocos2d::CCSprite* PlayerLeft;
 };
 
 #endif  // __HELLOWORLD_SCENE_H__
